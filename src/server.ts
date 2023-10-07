32725894f2f8f7bd7d406f48283a83d12cdad194
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-10-07 16:44:07 -0300

    feat: install and configure knex

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 // DEPENDENCY
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
+
+app.get('/tables', async () => {
+  const response = await knex('sqlite_schema').select('*')
+
+  return response
+})
 
 try {
   app
