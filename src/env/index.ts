c734475d0c43922d7fcf9b40307f4ab9f64fbbb2
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-10-08 19:45:46 -0300

    ci: create a new database for test environment

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,15 @@
 // DEPENDENCY
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+switch (process.env.NODE_ENV) {
+  case 'test':
+    config({ path: '.env.test' })
+    break
+  default:
+    config()
+    break
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,8 @@
 // DEPENDENCY
-import { describe, expect, it, beforeAll, afterAll } from 'vitest'
+import { describe, expect, it, beforeAll, afterAll, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -10,6 +11,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
