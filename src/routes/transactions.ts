490f17b75dfec1b5709768b5d2ec6bc20a09acdc
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-10-07 23:54:30 -0300

    feat: implement create transaction endpoint
    Signed-off-by: Davyd Souza <davyd.eduardo.souza@hotmail.com>

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,5 +1,6 @@
 // DEPENDENCY
 import { type FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '@/database'
 
 export async function transactionsRoute(app: FastifyInstance) {
@@ -7,5 +8,22 @@ export async function transactionsRoute(app: FastifyInstance) {
     const transactions = await knex('transactions').select('*')
 
     return transactions
+  })
+
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['income', 'outcome']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transaction').insert({
+      title,
+      amount: type === 'income' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
